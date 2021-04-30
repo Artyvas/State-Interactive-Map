fe88be8bc97b82e9882a6c5fde3093ecf4a8fedb
“Artyvas” <artemvasily@gmail.com>
2021-04-29 18:02:59 -0700

    adds script for parsing CSV file and loading into database via seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,18 @@ require "csv"
 
 puts "Creating States...."
 
-csv_text = File.read(Rails.root.join("db", "abbreviations.csv"))
+csv_text = File.read(Rails.root.join("db", "complete_state_data.csv"))
 
-csv = CSV.parse(csv_text, :headers => true, quote_char: "\x00")
+csv = CSV.parse(csv_text, :headers => true, encoding: "ISO-8859-1")
 
 data = csv.map do |row|
   {
-    name: row["State"],
+    name: row["state"],
     code: row["Code"],
+    med_inc: row["median_household_income"],
+    per_unemp_seas: row["share_unemployed_seasonal"],
+    per_pop_metro: row["share_population_in_metro_areas"],
+    per_pop_high: row["share_population_with_high_school_degree"],
     created_at: Time.now,
     updated_at: Time.now,
   }
